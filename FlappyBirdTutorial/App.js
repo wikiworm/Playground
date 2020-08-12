f1c22f04e90fed9cfcfccea40231cf97746361d6
Brandon Ripley <wikiwormdaily@gmail.com>
2020-08-11 18:37:26 -0700

    added touch events, pipes

--- FlappyBirdTutorial/App.js
+++ FlappyBirdTutorial/App.js
@@ -4,6 +4,26 @@ import Matter from "matter-js";
 import { GameEngine } from "react-native-game-engine";
 import Bird from './Bird';
 import Constants from './Constants';
+import Physics from './Physics';
+import Wall from './Wall'
+
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const generatePipes = () => {
+    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+
+    let sizes = [topPipeHeight, bottomPipeHeight]
+
+    if (Math.random() < 0.5) {
+        sizes = sizes.reverse();
+    }
+
+
+    return sizes;
+}
 
 
 export default class App extends Component {
@@ -24,13 +44,29 @@ export default class App extends Component {
         let world = engine.world;
 
         let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+        let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+        let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
 
-        Matter.World.add(world, [bird]);
+        let [firstPipeTopHeight, firstPipeBotHeight] = generatePipes();
+        let firstPipeTop = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), firstPipeTopHeight / 2, Constants.PIPE_WIDTH, firstPipeTopHeight, { isStatic: true });
+        let firstPipeBot = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (firstPipeBotHeight / 2), Constants.PIPE_WIDTH, firstPipeBotHeight, { isStatic: true });
+
+        let [secondPipeTopHeight, secondPipeBotHeight] = generatePipes();
+        let secondPipeTop = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), secondPipeTopHeight / 2, Constants.PIPE_WIDTH, secondPipeTopHeight, { isStatic: true });
+        let secondPipeBot = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (secondPipeBotHeight / 2), Constants.PIPE_WIDTH, secondPipeBotHeight, { isStatic: true });
+
+        Matter.World.add(world, [bird, floor, ceiling, firstPipeTop, firstPipeBot, secondPipeTop, secondPipeBot]);
 
 
         return {
             physics: { engine: engine, world: world },
             bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird},
+            floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+            ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+            firstPipeTop: { body: firstPipeTop, size: [Constants.PIPE_WIDTH, firstPipeTopHeight], color: "green", renderer: Wall },
+            firstPipeBot: { body: firstPipeBot, size: [Constants.PIPE_WIDTH, firstPipeBotHeight], color: "green", renderer: Wall },
+            secondPipeTop: { body: secondPipeTop, size: [Constants.PIPE_WIDTH, secondPipeTopHeight], color: "green", renderer: Wall },
+            secondPipeBot: { body: secondPipeBot, size: [Constants.PIPE_WIDTH, secondPipeBotHeight], color: "green", renderer: Wall }
         }
     }
 
@@ -42,6 +78,7 @@ export default class App extends Component {
                     ref={(ref) => { this.gameEngine = ref; }}
                     style={styles.gameContainer}
                     running={this.state.running}
+                    systems={[Physics]}
                     entities={this.entities}>
                 </GameEngine>
             </View>
